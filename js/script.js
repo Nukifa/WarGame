f7a84f6bca586efc45d97adfeb5e219d837d3791
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2023-10-05 22:59:21 +0200

    Js work, Quote on index

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function getQuote(){
     let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(xhttp.responseText);
+            document.getElementById("paragrapheCitationDiv").innerHTML = xhttp.responseText;
+        }
+    }
+    xhttp.open("GET", "../php/getQuoteFromApi.php?site=localhost/php/theQuoteAPI.php&cmd=getQuote");
+    xhttp.send();
 }
+getQuote();
