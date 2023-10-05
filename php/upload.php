1fa261f6b5862da586d027fd2e9429f62d956ebf
Rayot Paul <rayot.paul@gmail.com>
2023-10-05 16:15:02 +0200

    creation page upload

--- php/upload.php
+++ php/upload.php
@@ -1,27 +1,27 @@
 <?php
+if(isset($_POST['envoyer'])) {
+    // Vérifier si le fichier a été téléchargé avec succès
+    if (isset($_FILES['file']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
 
-// Vérifier si le fichier a été téléchargé avec succès
-if (isset($_FILES['file']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
+        // Obtenir l'extension du fichier
+        $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 
-    // Obtenir l'extension du fichier
-    $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+        // Vérifier si l'extension du fichier est autorisée
+        $extensions_autorisees = array('pdf', 'jpg', 'png', 'gif');
+        if (!in_array($extension, $extensions_autorisees)) {
+            echo 'L\'extension du fichier n\'est pas autorisée.';
+            exit();
+        }
 
-    // Vérifier si l'extension du fichier est autorisée
-    $extensions_autorisees = array('jpg', 'png', 'gif');
-    if (!in_array($extension, $extensions_autorisees)) {
-        echo 'L\'extension du fichier n\'est pas autorisée.';
-        exit();
+        // Déplacer le fichier vers le répertoire de destination
+        $destination = 'uploads/' . $_FILES['file']['name'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $destination);
+
+        // Afficher un message de succès
+        echo 'Le fichier a été téléchargé avec succès.';
+    } else {
+        // Afficher un message d'erreur
+        echo 'Le fichier n\'a pas été téléchargé avec succès.';
     }
-
-    // Déplacer le fichier vers le répertoire de destination
-    $destination = 'uploads/' . $_FILES['file']['name'];
-    move_uploaded_file($_FILES['file']['tmp_name'], $destination);
-
-    // Afficher un message de succès
-    echo 'Le fichier a été téléchargé avec succès.';
-} else {
-    // Afficher un message d'erreur
-    echo 'Le fichier n\'a pas été téléchargé avec succès.';
 }
-
 ?>
