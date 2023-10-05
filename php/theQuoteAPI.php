b42a837b9f916709d441f75e4525aa0856e038a2
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2023-10-05 16:40:43 +0200

    API Corrections

--- php/theQuoteAPI.php
+++ php/theQuoteAPI.php
@@ -1,5 +1,5 @@
 <?php
-    quotes_array = array(
+    $quotes_array = array(
         "La médecine a beau faire des progrès tous les jours, on n'a encore rien trouvé contre la connerie. À voir le nombre de gens atteints, ça mériterait pourtant qu'on vote des crédits. - Marie-Sabine Roger",
         "La médecine a fait tellement de progrès que plus personne n'est en bonne santé. - Aldous Huxley",
         "Il n'est pas bon, pour un médecin, d'admettre qu'il ne se sent pas bien. - Herbert George Wells",
@@ -13,7 +13,7 @@
     );
     if(!empty($_GET['cmd']) && !empty($_GET['token'])){
         if($_GET['cmd'] == "getQuote"){
-        echo quotes_array[random(0, sizeof(quotes_array)-1)];
+        echo $quotes_array[rand(0, sizeof($quotes_array)-1)];
         }else if($_GET['cmd'] == "getFlag" && (!isset($_GET['noflag']) || $_GET['noflag'] == "false")){
             echo "MAP{Ca_c_est_un_beau_flag}";
         }
