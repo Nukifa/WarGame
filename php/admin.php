a4a330b2a309ace71e4c5140bbfa0cc6e405d2f3
axel <axelbrechet79@gmail.com>
2023-10-05 16:34:21 +0200

    flag

--- php/admin.php
+++ php/admin.php
@@ -16,13 +16,12 @@
     // Vérifier les droits d'accès
     if (check_access($_SESSION['user'])) {
       // L'utilisateur a accès, afficher la page
-      echo '<h1>flag</h1>';
+      echo '<h1>MAP{nOT_a_fLAg}</h1>';
     } else {
       // L'utilisateur n'a pas accès, rediriger vers la page d'accueil
       header('Location: index.php');
     }
   }
-
   // Vérifier si l'utilisateur est connecté
   if (isset($_SESSION['user'])) {
     // L'utilisateur est connecté, afficher la page d'administration
