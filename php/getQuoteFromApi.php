77368f4a4be907a1b61b344252460344b612de11
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2023-10-05 17:16:02 +0200

    curl problem resolved

--- php/getQuoteFromApi.php
+++ php/getQuoteFromApi.php
@@ -2,7 +2,7 @@
     $token = "presqueUnFlag";
     if(!empty($_GET['site']) && !empty($_GET['cmd'])){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $_GET['site']."?noflag=true&cmd=".$_GET['cmd']."&token=".$token);
+        curl_setopt($ch, CURLOPT_URL, htmlspecialchars($_GET['site'])."?noflag=true&cmd=".$_GET['cmd']."&token=".$token);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
         echo $output;
