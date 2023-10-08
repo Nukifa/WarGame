dc5ee6d82d651a37907121c77ab25cbe8baedae2
axel <axelbrechet79@gmail.com>
2023-10-08 16:42:05 +0200

    on a besoin d'un index.php

--- php/admin.php
+++ php/admin.php
@@ -1,33 +1,21 @@
 
 <?php
-  // Fonction pour vérifier les droits d'accès
   function check_access($user) {
-    // Si l'utilisateur est un administrateur, il a accès
     if ($user['admin'] == 'true') {
       return true;
     }
-
-    // Sinon, l'utilisateur n'a pas accès
     return false;
   }
-
-  // Fonction pour afficher la page d'administration
   function show_admin_page() {
-    // Vérifier les droits d'accès
     if (check_access($_SESSION['user'])) {
-      // L'utilisateur a accès, afficher la page
       echo '<h1>MAP{nOT_a_fLAg}</h1>';
     } else {
-      // L'utilisateur n'a pas accès, rediriger vers la page d'accueil
       header('Location: index.php');
     }
   }
-  // Vérifier si l'utilisateur est connecté
   if (isset($_SESSION['user'])) {
-    // L'utilisateur est connecté, afficher la page d'administration
     show_admin_page();
   } else {
-    // L'utilisateur n'est pas connecté, rediriger vers la page de connexion
     header('Location: login.php');
   }
 ?>
--- php/login.php
+++ php/login.php
@@ -1,49 +1,28 @@
 <?php
-  // Vérifier si l'utilisateur est déjà connecté
   if (isset($_SESSION['user'])) {
-    // L'utilisateur est déjà connecté, rediriger vers la page d'accueil
-    header('Location: index.php');
+    header('Location: index.html');
     exit;
   }
 
   function get_user_by_username($username) {
-    // Connexion à la base de données
     $db = new PDO('sql:host=localhost;dbname=medical_site;charset=utf8', 'postgres', 'postgres');
   
     $login = $_POST['login'];
     $password = $_POST['password'];
-
-    // Préparez la requête SQL
     $requete = $db->query("SELECT * FROM utilisateurs WHERE login = $login AND password = $password");
-    // Exécution de la requête SQL
-  
-    // Récupération des résultats de la requête
     $user = $requete->fetch(PDO::FETCH_ASSOC);
-  
-    // Fermeture de la connexion à la base de données
     $db = null;
-  
-    // Retour de l'objet utilisateur
     return $user;
   }
-  
-  // Vérifier si le formulaire a été soumis
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Récupération des données du formulaire
     $username = $_POST['login'];
     $password = $_POST['password'];
-
-    // Vérifier les informations d'identification
     $user = get_user_by_username($username);
     if ($user && password_verify($password, $user['password'])) {
-      // Les informations d'identification sont valides, authentifier l'utilisateur
       $_SESSION['user'] = $user;
-
-      // Rediriger vers la page d'accueil
-      header('Location: index.php');
+      header('Location: index.html');
       exit;
     } else {
-      // Les informations d'identification sont invalides, afficher un message d'erreur
       $error = 'Les informations d\'identification ne sont pas correctes.';
     }
   }
