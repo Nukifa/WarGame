5897ad10d962e6247bc443e22e090b75a88d7e56
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2023-10-09 16:04:18 +0200

    DB work

--- sql/database.sql
+++ sql/database.sql
@@ -5,25 +5,23 @@ CREATE DATABASE medical_site;
 \c medical_site;
 
 -- Créer la table userssite
-CREATE TABLE user (
-  login VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,site
-  mail VARCHAR(255) NOT NULL,
-  admin BOOLEAN NOT NULL,
-  PRIMARY KEY (login)
+CREATE TABLE users (
+  login VARCHAR(255) NOT NULL PRIMARY KEY,
+  password VARCHAR(255) NOT NULL,
+  mail VARCHAR(255) UNIQUE NOT NULL,
+  admin BOOLEAN NOT NULL
 );
 
 -- Créer la table ordonnance
 CREATE TABLE ordonnance (
-  id INT NOT NULL AUTO_INCREMENT,
-  user VARCHAR(255) NOT NULL,
+  id SERIAL PRiMARY KEY,
+  login VARCHAR(255) NOT NULL,
   nom VARCHAR(255) NOT NULL,
   medicament VARCHAR(255) NOT NULL,
   frequence VARCHAR(255) NOT NULL,
   date DATE NOT NULL,
   nom_medecin VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT user_ordonance_fk FOREIGN KEY(user) REFERENCES user(login)
+  CONSTRAINT users_ordonance_fk FOREIGN KEY(login) REFERENCES users(login)
 );
 
 -- Ajouter quelques données à la table users
@@ -33,7 +31,7 @@ VALUES
 ('janedoe', '654321', 'janedoe@example.com', TRUE);
 
 -- Ajouter quelques données à la table ordonnance
-INSERT INTO ordonnance (nom, date, nom_medecin)
+INSERT INTO ordonnance (login, nom, medicament, frequence, date, nom_medecin)
 VALUES
 ('johndoe', 'Ordonnance 1', 'doliprane', '2x par jour midi et soir', '2023-07-20', 'Dr. Dupont'),
 ('johndoe', 'Ordonnance 2', 'smecta', 'tous les soirs pendant 3 jours', '2023-08-03', 'Dr. Martin');
