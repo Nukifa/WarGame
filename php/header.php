e58d183fdf89bba92552ed8dd2b0adcf431a47b3
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2023-10-09 15:17:32 +0200

    header locations updated

--- php/header.php
+++ php/header.php
@@ -2,13 +2,12 @@
     <h1>Site Web Médical</h1>
     <nav>
         <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Ordonnance</a></li>
+            <li><a href="../index.php">Accueil</a></li>
             <li><a href="#">Contact</a></li>
             <?php if(isset($_SESSION['user'])){ ?>
                 <li><a href="../php/disconnect.php">Déconnexion</a></li>
                 <li><a href="../php/admin.php">Admin</a></li>
-                <li><a href="upload.html">Télécharger un document</a></li>
+                <li><a href="../upload.php">Télécharger un document</a></li>
             <?php }else{ ?>
                 <li><a href="../php/login.php">Connexion</a></li>
             <?php } ?>
