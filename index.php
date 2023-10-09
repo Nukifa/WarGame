2a852b64b828004658f67cac894245ee8c64125a
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2023-10-09 15:06:46 +0200

    Affichage ordonances index.php. BDD et tableau d'affichage a revoir

--- index.php
+++ index.php
@@ -17,35 +17,40 @@ session_start();
     <section class="ordonnance">
       <h2>Ordonnance</h2>
       <?php #Affichage utilisateur connecté
-       if(isset($_SESSION['user'])){ ?>
-
-      <?php #Affichage utilisateur non connecté
-      }else{?>
-        <p>Vous devez vous connecter pour avoir accès à vos ordonances. connectez-vous en cliquant <a href="php/login.php" class="special-link">ici</a></p>
-      <?php }?>
-      <table>
-        <tr>
-          <th>Date</th>
-          <th>Médecin</th>
-          <th>Médicament</th>
-          <th>Posologie</th>
-          <th>Durée</th>
-        </tr>
-        <tr>
-          <td>2023-07-20</td>
-          <td>Dr. Dupont</td>
-          <td>Aspirine</td>
-          <td>2 comprimés par jour</td>
-          <td>10 jours</td>
-        </tr>
-        <tr>
+        if(isset($_SESSION['user'])){
+          #Récupération des ordonances de l'utilisateur
+          $db = new PDO('sql:host=localhost;dbname=medical_site;charset=utf8', 'postgres', 'postgres');
+          $request = $db->prepare("SELECT * FROM ordonance WHERE user = ?");
+          $request->execute(array($_SESSION['user']));
+          echo "<table>
+          <tr>
+            <th>Date</th>
+            <th>Médecin</th>
+            <th>Médicament</th>
+            <th>Posologie</th>
+            <th>Durée</th>
+          </tr>";
+          while($result = $request->fetch()){
+            echo "<tr>";
+            echo "<td>" . $result['nom'] . "</td>";
+            echo "<td>" . $result['date'] . "</td>";
+            echo "<td>" . $result['nom_medecin'] . "</td>";
+            echo "<tr>";
+          }
+          echo "</table>";
+        }else{?>
+          <p>Vous devez vous connecter pour avoir accès à vos ordonances. connectez-vous en cliquant <a href="php/login.php" class="special-link">ici</a></p>
+        <?php }?>
+      
+        
+        <!--<tr>
           <td>2023-08-03</td>
           <td>Dr. Martin</td>
           <td>Antibiotiques</td>
           <td>1 gélule par jour</td>
           <td>7 jours</td>
-        </tr>
-      </table>
+        </tr>-->
+      
     </section>
   </main>
   <footer>
